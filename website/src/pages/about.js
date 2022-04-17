8cc3a3cd39425065b802bd4894da6f76ecc19489
markser <markser.school@gmail.com>
2022-04-17 09:52:22 -0700

    delete the cache

---
+++ website/src/pages/about.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const About = () => {
+return (
+	<div>
+	<h1>
+		I am a capstone project
+		Kubernetes is the name.
+		What is your name?
+	</h1>
+	</div>
+);
+};
+
+export default About;
